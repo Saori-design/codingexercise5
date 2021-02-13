4459fce396c13fb5e1b613754cc79c696dc1bdac
Saori-design <65802428+Saori-design@users.noreply.github.com>
2021-02-12 22:29:07 -0500

    First commit.

---
+++ script.js
@@ -0,0 +1,16 @@
+$(window).on("load", start);
+
+function start() {
+
+    $('#slideshow').slick({
+        // setting-name: setting-value
+        prevArrow: null,
+        nextArrow: null,
+        autoplay: true,
+        fade: true
+      });
+ScrollReveal().reveal('.reveal1');
+ScrollReveal().reveal('.reveal2', { delay: 500 });
+ScrollReveal().reveal('.reveal3', { delay: 1000 });
+
+}
